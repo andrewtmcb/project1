dcae290b4587eb9d49b5638c063aa02f7f6d7b3d
Andy McBrayer <andrew.mcbrayer1@gmail.com>
2019-04-04 23:52:48 -0400

    oops forgot this

--- qs/src/main/java/com/revature/doa/ReimbursementDOAImpl.java
+++ qs/src/main/java/com/revature/doa/ReimbursementDOAImpl.java
@@ -1,8 +1,12 @@
 package com.revature.doa;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.revature.pojos.Reimbursement;
+import com.revature.services.ConnectionFactory;
 
 public class ReimbursementDOAImpl implements ReimbursementDOA {
 
@@ -32,7 +36,37 @@ public class ReimbursementDOAImpl implements ReimbursementDOA {
 
 	@Override
 	public void createReimbursement(Reimbursement reimb) {
-		// TODO Auto-generated method stub
+		
+		Connection conn = ConnectionFactory.getConnection();
+		try {
+			String insertStatement =
+			    "insert into proj1.rembursment_table(status ,userid ,eventdatetime ,address ,city ,state ,zip ,description ,gradingformat , \"cost\" , missingworktime, createddatetime, eventtype) " +
+			     "values (? ,? ,?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+			
+			conn.setAutoCommit(false);
+			PreparedStatement ps = conn.prepareStatement(insertStatement);
+			ps.setInt(1, reimb.getStatus());
+			ps.setInt(2, reimb.getUserId());
+			ps.setString(3, reimb.getEventDateTime());
+			ps.setString(4, reimb.getAddress());
+			ps.setString(5, reimb.getCity());
+			ps.setString(6, reimb.getState());
+			ps.setString(7, reimb.getZip());
+			ps.setString(8, reimb.getDescription());
+			ps.setString(9, reimb.getGradingformat());
+			ps.setFloat(10, (float) reimb.getCost());
+			ps.setBoolean(11, reimb.isMissingWorkTime());
+			ps.setString(12, reimb.getCreatedDateTime());
+			ps.setString(13, reimb.getEventType());
+			ps.executeUpdate();
+			conn.commit();
+			conn.close();
+	
+			} catch (SQLException e) {
+	
+				e.printStackTrace();
+			}
+	
 
 	}
 
